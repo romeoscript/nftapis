193236416a00a8e3edea73139fbd83f21e1c21ea
romeoscript <romeobourne211@gmail.com>
2023-10-16 17:09:28 +0100

    create doc

--- routes/CreateNft.js
+++ routes/CreateNft.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 import { authenticateUser } from "../middleware/auth.js";
 import cloudinary from "../lib/cloudinary.js";
-import fs from 'fs';
+import fs from "fs";
 import upload from "../middleware/upload.js";
 
 const db = new PrismaClient();
@@ -10,11 +10,73 @@ const db = new PrismaClient();
 // Load environment variables
 dotenv.config();
 
+/**
+ * @swagger
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ *
+ * /createNft:
+ *   post:
+ *     tags:
+ *       - NFT
+ *     description: Create a new NFT
+ *     security:
+ *       - bearerAuth: [] 
+ *     requestBody:
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               blockchain:
+ *                 type: string
+ *               image:
+ *                 type: string
+ *                 format: binary
+ *             required:
+ *               - name
+ *               - description
+ *               - blockchain
+ *               - image
+ *     responses:
+ *       201:
+ *         description: Successfully created the NFT
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 name:
+ *                   type: string
+ *                 image:
+ *                   type: string
+ *                 description:
+ *                   type: string
+ *                 blockchain:
+ *                   type: string
+ *                 userId:
+ *                   type: string
+ *       400:
+ *         description: Bad request - Missing required fields or No image uploaded
+ *       401:
+ *         description: Authentication failed
+ *       500:
+ *         description: Internal Server Error
+ */
+
 export async function createNft(req, res) {
   try {
     // Authenticate the user
     await authenticateUser(req, res);
- 
+
     // Apply multer middleware to handle file uploads
     await new Promise((resolve, reject) => {
       upload.single("image")(req, res, (error) => {
@@ -25,7 +87,7 @@ export async function createNft(req, res) {
         }
       });
     });
- 
+
     if (!req.file) {
       return res.status(400).json({ message: "No image uploaded." });
     }
@@ -44,14 +106,18 @@ export async function createNft(req, res) {
     }
 
     const result = await new Promise((resolve, reject) => {
-      cloudinary.uploader.upload(req.file.path, { folder: "NftImages" }, (error, result) => {
-        if (error) {
-            console.error("Cloudinary Error:", error); 
-          reject(error);
-        } else {
-          resolve(result);
+      cloudinary.uploader.upload(
+        req.file.path,
+        { folder: "NftImages" },
+        (error, result) => {
+          if (error) {
+            console.error("Cloudinary Error:", error);
+            reject(error);
+          } else {
+            resolve(result);
+          }
         }
-      });
+      );
     });
 
     // Delete the temporarily stored file
