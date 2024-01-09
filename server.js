6a0530e83a4681cbacf05b5466b05fedcb409cf3
romeoscript <romeobourne211@gmail.com>
2024-01-09 01:55:58 +0100

    doe

--- server.js
+++ server.js
@@ -6,48 +6,56 @@ import swaggerUi from "swagger-ui-express";
 const app = express();
 const PORT = 5000;
 
-import { getNFTs } from "./routes/nft.js";
-import { registerUser } from "./routes/registerUser.js";
-import { POST } from "./routes/loginUser.js";
-import { createNft } from "./routes/CreateNft.js";
-import { getUserNFTs } from "./routes/getusernft.js";
-import { buy } from "./routes/Buy.js";
-import { createStripeSession } from "./routes/stripePayment.js";
-import { getNFT } from "./routes/particularnft.js";
+// Import your route handlers
+import { getNFTs, registerUser, loginUser, createNft, getUserNFTs, buy, createStripeSession, getNFT } from "./routes";
 
+// Swagger configuration
 const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
     info: {
       title: "NFT API",
       version: "1.0.0",
-      description: "A simple Express NFT API",
+      description: "A simple Express NFT API"
     },
     servers: [
-      {
-        url: "http://localhost:5000",
-      },
-      { url: "https://nftapis.onrender.com" },
-    ],
+      { url: "http://localhost:5000" },
+      { url: "https://nftapis.onrender.com" }
+    ]
   },
-  apis: ["./routes/*.js"], // path to your route files
+  apis: ["./routes/*.js"] // path to your route files
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use(express.json());
-app.use(cors());
+// Use CORS with options
+app.use(cors({
+  origin: "*", // Allow all origins
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Allow cookies
+  allowedHeaders: "Content-Type,Authorization"
+}));
 
+// Enable pre-flight across-the-board
+app.options('*', cors()); // include before other routes
+
+// Swagger UI setup
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Define your API routes
 app.get("/nft", getNFTs);
 app.get("/mynfts", getUserNFTs);
 app.post("/buy/:tokenid", buy);
 app.get("/nft/:tokenid", getNFT);
 app.post("/register", registerUser);
-app.post("/login", POST);
+app.post("/login", loginUser);
 app.post("/creatNft", createNft);
 app.post("/create-stripe-session", createStripeSession);
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
